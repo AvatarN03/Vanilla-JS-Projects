a86340fa69fed370ab830f896e75572e6d39b68c
AvatarN03 <jsprashanth003@gmail.com>
2025-04-24 12:41:11 +0530

    deploy in netlify -6

--- webpack.config.js
+++ webpack.config.js
@@ -7,14 +7,14 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  mode: 'development',
+  mode: 'development', // or 'production' depending on your needs
   devServer: {
     static: './dist',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.html', // Make sure this is correct (root if your source is there)
-      filename: 'index.html',   // Ensure the output file is named 'index.html' in 'dist'
+      template: './index.html', // This now correctly points to index.html in the root
+      filename: 'index.html',   // This ensures the output index.html is also named index.html in the dist folder
     }),
   ],
 };
