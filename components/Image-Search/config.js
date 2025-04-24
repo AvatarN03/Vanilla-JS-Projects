aa0dbc099702675a2e4709bf925fd60efad36772
AvatarN03 <jsprashanth003@gmail.com>
2025-04-24 14:20:51 +0530

    the webpacked removed and reverted to normal esmodule run

--- components/Image-Search/config.js
+++
@@ -1,5 +0,0 @@
-// // config.js
-// window.env = {
-//     ACCESS_KEY:"wgdDprJp50WHK5H9l7oOjulZtH-F6iwHJRRHf4PGesE"
-//   };
-  
--- components/Random-Meme/config.js
+++
@@ -1,6 +0,0 @@
-// config.js
-window.env = {
-    API_BASE_URL: "https://meme-api.com/gimme/wholesomememes"
-    
-  };
-  
