58f0d4e0d46f2ad24c70e19ebbe0f5098f532dca
AvatarN03 <jsprashanth003@gmail.com>
2025-04-24 11:59:09 +0530

    Complete rechange the stucture by using the webpack

--- Animated-Search-Icon/main.js
+++
@@ -1,9 +0,0 @@
-const searchBar = document.querySelector(".search-bar-container");
-const magnifer = document.querySelector(".magnifer");
-const input = document.querySelector("input");
-
-magnifer.addEventListener("click", (e) => {
-  searchBar.classList.toggle("active");
-
-  input.focus();
-});
