863d63ed55dde3e67e97447af9a2805a92506bc5
AvatarN03 <jsprashanth003@gmail.com>
2025-04-24 12:13:42 +0530

    deplot attempt in netlify-2

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -10,5 +11,9 @@ module.exports = {
   devServer: {
     static: './dist',
   },
-  
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html', // Path to your main HTML file
+    }),
+  ],
 };
