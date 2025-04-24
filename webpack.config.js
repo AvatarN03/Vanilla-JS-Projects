122e02ce9702a7c97ba72b0c51e54b448f0b1f46
AvatarN03 <jsprashanth003@gmail.com>
2025-04-24 12:52:33 +0530

    deploy in netlify -7

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,34 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // If you're importing CSS in JS
 
 module.exports = {
+  mode: 'development', // Or 'production' for your final build
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  mode: 'development', // or 'production' depending on your needs
   devServer: {
     static: './dist',
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'], // If importing CSS in JS
+      },
+      // ... other rules ...
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.html', // This now correctly points to index.html in the root
-      filename: 'index.html',   // This ensures the output index.html is also named index.html in the dist folder
+      template: './index.html',
+      filename: 'index.html',
     }),
+    new MiniCssExtractPlugin({
+      filename: 'home.css', // Or your desired CSS filename
+    }),
+   
   ],
 };
